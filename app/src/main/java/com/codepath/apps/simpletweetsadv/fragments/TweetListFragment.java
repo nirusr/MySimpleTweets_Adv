665973e1f8ee68a8f395a1a09879aea351b41e14
Srinivasan Govindaraji <sgovindaraji@walmartlabs.com>
2015-10-30 15:21:44 -0700

    Basic-4 Added Swipe Logic

--- app/src/main/java/com/codepath/apps/simpletweetsadv/fragments/TweetListFragment.java
+++ app/src/main/java/com/codepath/apps/simpletweetsadv/fragments/TweetListFragment.java
@@ -2,37 +2,21 @@ package com.codepath.apps.simpletweetsadv.fragments;
 
 import android.app.Activity;
 import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
-import com.activeandroid.query.Delete;
-import com.codepath.apps.simpletweetsadv.Helper.DBSaveAsyncTask;
 import com.codepath.apps.simpletweetsadv.R;
 import com.codepath.apps.simpletweetsadv.adapters.TweetsArrayAdapter;
-import com.codepath.apps.simpletweetsadv.application.TwitterApplication;
-import com.codepath.apps.simpletweetsadv.client.TwitterClient;
-import com.codepath.apps.simpletweetsadv.listeners.EndlessScrollListener;
 import com.codepath.apps.simpletweetsadv.models.Tweet;
-import com.codepath.apps.simpletweetsadv.models.TweetModel;
-import com.loopj.android.http.JsonHttpResponseHandler;
-
-import org.apache.http.Header;
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by sgovind on 10/30/15.
@@ -50,9 +34,10 @@ public class TweetListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         //return super.onCreateView(inflater, container, savedInstanceState);
-        View v = inflater.inflate(R.layout.fragments_tweet_list, container, false);
+        View v = inflater.inflate(R.layout.fragment_tweet_list, container, false);
         lvTweets = (RecyclerView) v.findViewById(R.id.lvTweets);
         lvTweets.setLayoutManager(new LinearLayoutManager(getContext()));
+        swipeContainer = (SwipeRefreshLayout) v.findViewById(R.id.swipeContainer);
         return v;
     }
 
